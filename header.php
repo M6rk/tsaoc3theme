a7ab0f1ddaa2456fcaf2466458e8d8c828a7ee7d
Mark <mark.lovesey03@gmail.com>
2025-08-13 22:38:27 -0700

    basic header changes, updated screenshot, some block updates

--- header.php
+++ header.php
@@ -4,6 +4,7 @@
 <head>
   <meta charset="<?php bloginfo('charset'); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Okanagan Central Salvation Army</title>
   <?php wp_head(); ?>
 </head>
 
